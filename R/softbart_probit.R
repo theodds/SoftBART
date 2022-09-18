8d2909ba75643c2701f152cc167d33d7801edabd
Antonio Linero <theodds@gmail.com>
2022-09-18 15:31:55 -0500

    WOrking on probit

--- R/softbart_probit.R
+++ R/softbart_probit.R
@@ -1,5 +1,5 @@
-softbart_probit <- function(formula, data, test_data, 
-                            hypers = NULL, opts = NULL) {
+softbart_probit <- function(formula, data, test_data, num_tree = 20,
+                            k = 2, hypers = NULL, opts = NULL) {
   
   ## Get design matricies
   
@@ -16,8 +16,19 @@ softbart_probit <- function(formula, data, test_data,
   ## Set up hypers
   
   if(is.null(hypers)) {
-    hypers <- Hypers(X = X_train, )
+    hypers <- Hypers(X = X_train, Y = Y_train)
   }
+  hypers$sigma_mu = 3 / k / sqrt(num_tree)
+  hypers$sigma <- 1
+  hypers$sigma_hat <- 1
+  hypers$num_tree <- num_tree
+  
+  ## Set up opts
+  
+  if(is.null(opts)) {
+    opts <- Opts()
+  }
+  opts$update_sigma <- FALSE
   
   ## Normalize!
   
@@ -36,6 +47,46 @@ softbart_probit <- function(formula, data, test_data,
   for(i in 1:ncol(X_train)) {
     X_train[,i] <- ecdfs[[i]](X_train)
     X_test[,i] <- ecdfs[[i]](X_test)
+  }
+  
+  ## Make forest ----
+  
+  probit_forest <- MakeForest(hypers, opts)
+  
+  ## Initialize Z
+  
+  mu <- probit_forest$do_predict(X_train)
+  lower <- ifelse(Y_train == 0, -Inf, 0)
+  upper <- ifelse(Y_train == 0, 0, Inf)
+  
+  ## Initialize output
+  
+  mu_train <- matrix(NA, nrow = opts$num_save, ncol = length(Y_train))
+  mu_test  <- matrix(NA, nrow = opts$num_save, ncol = length(Y_test))
+  sigma_mu <- numeric(opts$num_save)
+  
+  
+  ## Warmup
+  
+  for(i in 1:opts$num_burn) {
+    ## Sample Z
+    Z <- rtruncnorm(n = length(Y_train), a = lower, b = upper, 
+                    mean = mu, sd = 1)
+    ## Update R
+    mu <- probit_forest$do_gibbs(X_train, Z, X_train, 1)
+  }
+  
+  ## Save
+  
+  for(i in 1:opts$num_save) {
+    for(j in 1:opts$num_thin) {
+      ## Sample Z
+      Z <- rtruncnorm(n = length(Y_train), a = lower, b = upper, 
+                      mean = mu, sd = 1)
+      ## Update R
+      mu <- probit_forest$do_gibbs(X_train, Z, X_train, 1)
+    }
+    
   }
   
 }
